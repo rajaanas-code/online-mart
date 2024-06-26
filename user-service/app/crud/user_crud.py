56dddd31ae913923fc8a35ea68fb44c2f7c13acd
Raja Anas Turk <rajaanasturk157@gmail.com>
2024-06-26 15:56:26 +0500

    @RajaAnasTurk

--- user-service/app/crud/user_crud.py
+++ user-service/app/crud/user_crud.py
@@ -1,9 +1,13 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Security
 from sqlmodel import Session, select
 from app.models.user_model import UserService
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from app import settings
 
 
 def create_user(user_data: UserService, session:Session):
+    hashed_password = get_password_hash(user_data.password)
     session.add(user_data)
     session.commit()
     session.refresh(user_data)
