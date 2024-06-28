54591477c2a3bd3edf8d163e831b98a710e75246
Raja Anas Turk <rajaanasturk157@gmail.com>
2024-06-28 17:31:48 +0500

    ..

--- user-service/app/settings.py
+++ user-service/app/settings.py
@@ -5,4 +5,7 @@ config = Config(".env")
 
 DATABASE_URL = config("DATABASE_URL", cast=Secret)
 KAFKA_USER_TOPIC = config("KAFKA_USER_TOPIC", cast=str)
+
+BOOTSTRAP_SERVER = config("BOOTSTRAP_SERVER", cast=str)
+
 SECRET_KEY = config("SECRET_KEY", cast=Secret)
