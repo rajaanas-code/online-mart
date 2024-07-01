442ec41c399b80203c5843304eda9d2ceb2f1aa5
Raja Anas Turk <rajaanasturk157@gmail.com>
2024-07-01 13:08:26 +0500

    user service changes

--- user-service/app/models/user_model.py
+++ user-service/app/models/user_model.py
@@ -5,8 +5,6 @@ class UserService(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str
     email: str = Field(index=True, unique=True, nullable=False)
-    cell_number: int
     hashed_password: str
-    full_name: str
 
     
